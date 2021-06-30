0350ac3822eaca9411ca3a9c84df2b763c2b6ce2
arunbhalli <tomarbhalli@gmail.com>
2021-06-30 21:03:33 +0200

    adds show action for articles

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,4 +3,8 @@ class Api::ArticlesController < ApplicationController
     article = Article.all
     render json: { articles: article }
   end
+  def show
+    article=Article.find(params[:id]) 
+    render json: { article: article}
+  end
 end
